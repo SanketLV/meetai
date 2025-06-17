4529e6a49d31ab647725bbe3fd01a56f165ced02
SanketLV <sanketlakhani80@gmail.com>
2025-06-17 22:19:56 +0530

    feat: integrate TRPC with React Query; add API routes and context management

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,30 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const HomeViewPage = () => {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-
-  return (
-    <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session?.user.name}</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => router.push("/sign-in"),
-            },
-          })
-        }
-      >
-        Sign Out
-      </Button>
-    </div>
-  );
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "Sanket" }));
+  return <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>;
 };
 
 export default HomeViewPage;
