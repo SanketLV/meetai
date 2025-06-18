375540415b534f67b81c38d62edcaf9f53572ab5
SanketLV <sanketlakhani80@gmail.com>
2025-06-18 18:44:38 +0530

    feat: add agents management with TRPC integration; implement agents view, loading, and error states

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const HomeViewPage = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({ text: "Sanket" }));
-  return <div className="flex flex-col p-4 gap-y-4">{data?.greeting}</div>;
+  return <div>Home View</div>;
 };
 
 export default HomeViewPage;
